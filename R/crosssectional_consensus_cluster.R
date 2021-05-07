dc614c31c06577f75c27e4ba55df87149149c454
Jonas Hagenberg <jonas_hagenberg@psych.mpg.de>
2021-05-07 16:29:51 +0200

    add function to shortly provide an example for cross-sectional consensus clustering with categorical values

---
+++ R/crosssectional_consensus_cluster.R
@@ -0,0 +1,59 @@
+#' Cross-sectional clustering with categorical variables
+#'
+#' This function uses the \code{ConsensusClusterPlus} function from the package
+#' with the same name with defaults for clustering data with categorical
+#' variables.
+#'
+#' @param data a matrix or data.frame containing variables that should be used
+#' for computing the distance. This argument is passed to \code{StatMatch::gower.dist}
+#' @param reps number of repetitions, same as in \code{ConsensusClusterPlus}
+#' @param distance name of the distance function used; as we directly provide
+#' a distance object to the \code{d} argument of \code{ConsensusClusterPlus},
+#' this argument has no function
+#' @param finalLinkage linkage method for final clustering,
+#' same as in \code{ConsensusClusterPlus}same as in \code{ConsensusClusterPlus}
+#' @param innerLinkage linkage method for clustering steps,
+#' same as in \code{ConsensusClusterPlus}
+#' @param ... other arguments passed to \code{ConsensusClusterPlus}, attention:
+#' the \code{d} argument can \bold{not} be set as it is directly computed by
+#' \code{crosssectional_consensus_cluster}
+#'
+#' @return The output is produced by \code{ConsensusClusterPlus}
+#'
+#' @importFrom stats as.dist
+#' @export
+#'
+#' @examples
+#' dc <- mtcars
+#' # scale continuous variables
+#' dc <- sapply(mtcars[, 1:7], scale)
+#' # code factor variables
+#' dc <- cbind(as.data.frame(dc),
+#'             vs = as.factor(mtcars$vs),
+#'             am = as.factor(mtcars$am),
+#'             gear = as.factor(mtcars$gear),
+#'             carb = as.factor(mtcars$carb))
+#' cc <- crosssectional_consensus_cluster(
+#'   data = dc,
+#'   reps = 10,
+#'   seed = 1
+#' )
+crosssectional_consensus_cluster <- function(
+  data,
+  reps = 1000,
+  distance = "StatMatch::gower.dist",
+  finalLinkage = "ward.D2",
+  innerLinkage = "ward.D2",
+  ...) {
+
+  data_as_distance <- as.dist(StatMatch::gower.dist(data))
+  consensus_cluster <- ConsensusClusterPlus::ConsensusClusterPlus(
+    d = data_as_distance,
+    reps = reps,
+    distance = distance,
+    finalLinkage = finalLinkage,
+    innerLinkage = innerLinkage,
+    ...)
+
+  consensus_cluster
+}
